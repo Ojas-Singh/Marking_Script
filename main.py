9c11750e9d5fab13719edec63e016a9a71ab26d6
Ojas_singh <singhojas007@gmail.com>
2023-11-25 21:29:49 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,69 +1,81 @@
 import streamlit as st
 
-# Define a function to create a feedback table
-def create_feedback_table(feedback_data):
-    feedback_table = "**CH202 2021 Beer Lambert Law Assessment Feedback**\n\n"
-    feedback_table += "**Student Name:**\n"
-    feedback_table += "**Student ID:**\n\n"
-    feedback_table += "| Section                | Marks Awarded | Maximum Marks | Feedback                          |\n"
-    feedback_table += "|------------------------|---------------|---------------|-----------------------------------|\n"
-
-    total_marks = 0
-    for section, data in feedback_data.items():
-        feedback_table += f"| {section} | {data['marks']} | {data['max_marks']} | {data['feedback']} |\n"
-        total_marks += data['marks']
-    
-    feedback_table += f"| **Total** | **{total_marks}** | **9** | **Overall Feedback:** {feedback_data['Total']['feedback']} |\n\n"
-    feedback_table += "**Comments:**\n\n"
-    for section, data in feedback_data.items():
-        if section != "Total":
-            feedback_table += f"- **{section}:** {data['feedback']}\n"
-    feedback_table += "\n**Final Marks:**\n\n"
-
-    return feedback_table
-
-# Define the sections and their max marks
+# Define the sections and their maximum marks
 sections = {
     "TITLE AND DATE": 0.5,
     "AIM": 0.5,
-    "INTRODUCTION": 1,
+    "INTRODUCTION": 1.0,
     "PROCEDURE": 1.5,
-    "RESULTS": 2,
+    "RESULTS": 2.0,
     "ANALYSIS": 1.5,
-    "DETERMINATION & CALC.": 1,
-    "CONCLUSION": 1,
-    "QUESTIONS": 1,
-    "Total": {"max_marks": 9, "marks": 0, "feedback": ""}  # Total is handled separately
+    "DETERMINATION & CALC.": 1.0,
+    "CONCLUSION": 1.0,
+    "QUESTIONS": 1.0
 }
 
-# Streamlit app
-def main():
-    st.title("CH202 2021 Beer Lambert Law Assessment Feedback Generator")
+# Dummy feedback for each section
+dummy_feedback = {
+    "TITLE AND DATE": "Title and date appropriately provided.",
+    "AIM": "Aim is clearly defined.",
+    "INTRODUCTION": "Introduction provides necessary background.",
+    "PROCEDURE": "Procedure is detailed and follows past perfect tense.",
+    "RESULTS": "Results are well-presented with correct charts.",
+    "ANALYSIS": "Analysis includes correctly drawn graphs with labeled axes.",
+    "DETERMINATION & CALC.": "Calculations are accurate with proper units.",
+    "CONCLUSION": "Conclusion is concise and reflects the results.",
+    "QUESTIONS": "Questions are answered thoroughly."
+}
 
-    # Dictionary to hold the feedback data
-    feedback_data = {}
+# Function to create a well-aligned feedback table
+def create_feedback_table(marks_awarded, feedback_given, sections):
+    section_width = max(len(s) for s in sections) + 2  # Longest section name + padding
+    marks_width = 10  # "x.x/xx" format
+    feedback_width = 60  # Fixed width for feedback
 
-    # Iterate over sections to get input from the user
+    header = (
+        f"| {'Section'.ljust(section_width)} | "
+        f"{'Marks'.center(marks_width)} | "
+        f"{'Feedback'.ljust(feedback_width)} |\n"
+        f"|{'-' * section_width}|{'-' * marks_width}|{'-' * feedback_width}|\n"
+    )
+
+    rows = ""
+    for section, max_mark in sections.items():
+        marks_str = f"{marks_awarded[section]:.1f}/{max_mark:.1f}"
+        rows += (
+            f"| {section.ljust(section_width)} | "
+            f"{marks_str.center(marks_width)} | "
+            f"{feedback_given[section].ljust(feedback_width)} |\n"
+        )
+    
+    total_marks = sum(marks_awarded.values())
+    total_max = sum(sections.values())
+    total_marks_str = f"{total_marks:.1f}/{total_max:.1f}"
+    
+    footer = (
+        f"| {'Total'.ljust(section_width)} | "
+        f"{total_marks_str.center(marks_width)} | "
+        f"{' ' * feedback_width} |\n"
+        f"|{'=' * (section_width + marks_width + feedback_width + 5)}|\n"
+    )
+
+    feedback_table = header + rows + footer
+
+    return feedback_table
+
+# Streamlit form for input
+with st.form(key='feedback_form'):
+    st.title('Feedback Form')
+
+    marks_awarded = {}
+    feedback_given = {}
+
     for section, max_marks in sections.items():
-    with st.form(key=section):
-        st.subheader(section)
-        # Convert max_marks to float to match the type of min_value and step
-        marks = st.number_input(f"Enter marks for {section}", min_value=0.0, max_value=float(max_marks), step=0.1)
-        feedback = st.text_area(f"Enter feedback for {section}", help=f"Write the feedback for the {section} section.")
-        submit_button = st.form_submit_button(label='Save')
+        marks_awarded[section] = st.number_input(f"Marks for {section}", min_value=0.0, max_value=float(max_marks), step=0.1, format="%.1f")
+        feedback_given[section] = st.text_area(f"Feedback for {section}", value=dummy_feedback[section], height=100)
 
-            if submit_button:
-                feedback_data[section] = {"marks": marks, "max_marks": max_marks, "feedback": feedback}
+    submitted = st.form_submit_button("Submit")
 
-    # When all forms have been filled
-    if st.button('Generate Feedback Table'):
-        # If Total section has not been filled, prompt the user to fill it
-        if "Total" not in feedback_data:
-            st.error("Please fill in the 'Total' section.")
-        else:
-            # Create and display the feedback table
-            feedback_table = create_feedback_table(feedback_data)
-            st.text_area("Copy the feedback table below:", feedback_table, height=300)
-
-if __name__ == "__main__":
-    main()
+if submitted:
+    feedback_table = create_feedback_table(marks_awarded, feedback_given, sections)
+    st.code( feedback_table)
